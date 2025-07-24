21f87ba12f33a67be9b3427f9a7eadff0a152f4e
Pedro do PHP <pedrofalck2000@gmail.com>
2025-07-24 09:15:13 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,18 +83,6 @@
     @endforeach
   </div>
   @endif
-  @if(env('ENABLE_ADS_TERRA', false))
-  <script type="text/javascript">
-    atOptions = {
-      'key': "{{ env('ADS_TERRA_BANNER_CODE')}}",
-      'format': 'iframe',
-      'height': 90,
-      'width': 728,
-      'params': {}
-    };
-  </script>
-  <script type="text/javascript" src="//www.highperformanceformat.com/{{ env('ADS_TERRA_BANNER_CODE')}}/invoke.js"></script>
-  @endif
 </div>
 @endsection
 @push('scripts')
