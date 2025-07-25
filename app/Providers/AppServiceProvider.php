a9861aa4e3ea3eef2f7e2a89637703c147a17105
Pedro do PHP <pedrofalck2000@gmail.com>
2025-07-25 14:37:42 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,18 +11,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
+
         Paginator::useBootstrapFive();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
