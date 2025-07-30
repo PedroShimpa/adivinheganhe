508629a8a251faacca823971f87e7da74b009366
Pedro do PHP <pedrofalck2000@gmail.com>
2025-07-30 11:28:48 -0300

    Update preload.php

--- preload.php
+++ preload.php
@@ -2,3 +2,17 @@
 
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
+// Preload arquivos principais do Laravel
+$paths = [
+    __DIR__.'/bootstrap/app.php',
+    __DIR__.'/app/Providers/AppServiceProvider.php',
+    __DIR__.'/app/Http/Kernel.php',
+];
+
+foreach ($paths as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
