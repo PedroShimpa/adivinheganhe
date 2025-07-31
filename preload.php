15f00d5241caff608c4a9bb8626371dd9bf29655
Pedro do PHP <pedrofalck2000@gmail.com>
2025-07-31 16:32:57 -0300

    Update preload.php

--- preload.php
+++ preload.php
@@ -1,15 +1,15 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+// require __DIR__.'/vendor/autoload.php';
 
-$paths = [
-    __DIR__.'/bootstrap/app.php',
-    __DIR__.'/app/Providers/AppServiceProvider.php',
-    __DIR__.'/app/Http/Kernel.php',
-];
+// $paths = [
+//     __DIR__.'/bootstrap/app.php',
+//     __DIR__.'/app/Providers/AppServiceProvider.php',
+//     __DIR__.'/app/Http/Kernel.php',
+// ];
 
-foreach ($paths as $file) {
-    if (file_exists($file)) {
-        require_once $file;
-    }
-}
+// foreach ($paths as $file) {
+//     if (file_exists($file)) {
+//         require_once $file;
+//     }
+// }
