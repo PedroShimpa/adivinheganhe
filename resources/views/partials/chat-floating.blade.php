44be8ab24a39aec35ac22db1549daef3ac7c1d05
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-04 17:38:14 -0300

    Update chat-floating.blade.php

--- resources/views/partials/chat-floating.blade.php
+++ resources/views/partials/chat-floating.blade.php
@@ -135,7 +135,7 @@ $(function() {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content');,
+                    'X-CSRF-TOKEN': csrfToken
                     'Accept': 'application/json'
                 },
                 body: JSON.stringify({
