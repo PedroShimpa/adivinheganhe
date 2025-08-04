716dfb63375038792d4a10f3e200ff586baff640
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-04 08:47:48 -0300

    notificando o edmain

---
+++ app/Mail/NotifyAdminsOfNewTicket.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyAdminsOfNewTicket extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public string $nome;
+    public ?string $email;
+    public string $categoria;
+    public string $descricao;
+
+    public function __construct(string $nome, ?string $email, string $categoria, string $descricao)
+    {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->categoria = $categoria;
+        $this->descricao = $descricao;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Novo chamado aberto no sistema',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.new_ticket_notification',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
