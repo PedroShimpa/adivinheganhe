3f8fb8bee2bc70b98924cb388fac044073ab4168
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-04 17:45:57 -0300

    correção email

--- resources/views/emails/acerto_admin.blade.php
+++ resources/views/emails/acerto_admin.blade.php
@@ -1,5 +1,5 @@
 <p><strong>{{ $usuario->name }}</strong> ({{ $usuario->email }}) acertou a adivinhação:</p>
 
-<p><strong>"{{ $adivinhacao->pergunta }}"</strong></p>
+<p><strong>"{{ $adivinhacao->titulo }}"</strong></p>
 
 <p>A adivinhação foi marcada como resolvida.</p>
--- resources/views/emails/acerto_usuario.blade.php
+++ resources/views/emails/acerto_usuario.blade.php
@@ -1,5 +1,5 @@
 <p>Parabéns {{ $username }}!</p>
 
-<p>Você acertou a adivinhação: <strong>"{{ $adivinhacao->pergunta }}"</strong>.</p>
+<p>Você acertou a adivinhação: <strong>"{{ $adivinhacao->titulo }}"</strong>.</p>
 
 <p>Seu prêmio será processado em breve, fique atento em seu whatsapp por entraremos em contato por la.</p>
