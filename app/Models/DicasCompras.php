a2632473b025291ae183c5e0454a736ab22ff2d7
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-13 10:38:42 -0300

    add cachable to dicas

--- app/Models/DicasCompras.php
+++ app/Models/DicasCompras.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 
 class DicasCompras extends Model
 {
+    use Cachable;
+    
     protected $table = 'dicas_compras';
 
     protected $fillable = [
