83477548352bb0fa8620776263d21e78a42dab90
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-15 18:46:28 -0300

    liberado_at

---
+++ database/migrations/2025_08_15_184209_add_column_liberado_at_adivinhacoes.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::table('adivinhacoes', function (Blueprint $table) {
+            $table->timestamp('liberado_at')->nullable()->index();
+            $table->boolean('notificado_canal_whatsapp')->default(false)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
