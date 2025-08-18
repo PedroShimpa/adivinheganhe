d28357de7a22e922908397da031e40b497ff17a9
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-18 13:25:46 -0300

    ajuste de bugs

---
+++ resources/views/partials/modals.blade.php
@@ -0,0 +1,42 @@
+
+    <div class="modal fade" id="modalInformacoes" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content rounded-4 shadow-5">
+                <div class="modal-header border-0">
+                    <h5 class="modal-title" id="modalLabel">
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body" id="modalDescricao"></div>
+                <div class="modal-footer border-0">
+                    <button type="button" class="btn btn-secondary rounded-pill px-4" data-bs-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalSuasRespostas" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content rounded-4 shadow-5">
+                <div class="modal-header border-0">
+                    <h5 class="modal-title" id="">Seus Palpites
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body" id="">
+                    <table class="table">
+                        <thead>
+                            Palpite
+                        </thead>
+                        <tbody id="tbodyRespostas">
+
+                        </tbody>
+                    </table>
+
+                </div>
+                <div class="modal-footer border-0">
+                    <button type="button" class="btn btn-secondary rounded-pill px-4" data-bs-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
