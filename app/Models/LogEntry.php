57339bab47ff7fe4145e7e5f605b9a02f35b6880
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-22 09:49:59 -0300

    inclusão do log database

---
+++ app/Models/LogEntry.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogEntry extends Model
+{
+    protected $table = 'logs';
+
+    protected $fillable = [
+        'level',
+        'message',
+        'context',
+    ];
+
+    protected $casts = [
+        'context' => 'array',
+    ];
+}
--- config/logging.php
+++ config/logging.php
@@ -58,6 +58,14 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        'database' => [
+            'driver' => 'monolog',
+            'handler' => App\Logging\DatabaseHandler::class,
+            'with' => [
+                'level' => 'debug',
+            ],
+        ],
+
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
@@ -89,7 +97,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
