44c761651a7b271dec25fcdfc7a210a039c42d4f
Pedro do PHP <pedrofalck2000@gmail.com>
2025-08-29 10:51:59 -0300

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -30,7 +30,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('home', absolute: false));
+        return redirect()->intended(url()->previous());
     }
 
     /**
