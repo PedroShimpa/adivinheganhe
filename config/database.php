5c15ce4a348a8c4d837e375bd3dc9a720dee04a7
Pedro do PHP <pedrofalck2000@gmail.com>
2025-10-21 15:31:49 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -143,33 +143,44 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+    'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
-            'persistent' => env('REDIS_PERSISTENT', false),
+    'options' => [
+        'cluster' => env('REDIS_CLUSTER', 'redis'),
+        'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+    ],
+
+    'default' => [
+        'scheme' => env('REDIS_SCHEME', 'tcp'),
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'username' => env('REDIS_USERNAME', null),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => env('REDIS_DB', 0),
+
+        // ⚡️ Habilita SSL de forma segura para o CloudClusters
+        'ssl' => [
+            'cafile' => env('REDIS_SSL_CA', null), // aponta para /var/www/adivinheganhe/ca.crt
+            'verify_peer' => false,                 // desativa verificação rígida
+            'verify_peer_name' => false,
         ],
+    ],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+    'cache' => [
+        'scheme' => env('REDIS_SCHEME', 'tcp'),
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'username' => env('REDIS_USERNAME', null),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => env('REDIS_CACHE_DB', 1),
+
+        'ssl' => [
+            'cafile' => env('REDIS_SSL_CA', null),
+            'verify_peer' => false,
+            'verify_peer_name' => false,
         ],
+    ],
 
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-        ],
-
-    ]
-
+],
 
 ];
