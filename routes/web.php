f353fda4ffedd4dbc78942d6331a636fe8cff451
Pedro do PHP <pedrofalck2000@gmail.com>
2025-10-27 17:00:43 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -169,11 +169,7 @@ Route::middleware(['auth', 'banned', 'trackOnline'])->group(function () {
         Route::post('/suporte/admin/create-ticket', [SuporteController::class, 'adminCreateTicket'])->name('suporte.admin.create-ticket');
         Route::get('/suporte/admin/{suporte}', [SuporteController::class, 'adminShow'])->name('suporte.admin.show');
         Route::put('/suporte/admin/{suporte}', [SuporteController::class, 'adminUpdate'])->name('suporte.admin.update');
-
-        Route::get('/email-tracking', [EmailTrackingController::class, 'index'])->name('email_tracking.index');
-        Route::get('/email-tracking/open/{trackingId}', [EmailTrackingController::class, 'trackOpen'])->name('email_tracking.open');
-        Route::get('/email-tracking/click/{trackingId}', [EmailTrackingController::class, 'trackClick'])->name('email_tracking.click');
-
+Route::get('/email-tracking', [EmailTrackingController::class, 'index'])->name('email_tracking.index');
         #deleções
         Route::delete('/premiacoes/deletar/{premiacao}', [AdivinhacoesController::class, 'deletarPremiacao'])->name('premiacoes.delete');
         Route::post('/premiacoes/marcar-pago/{premiacao}', [AdivinhacoesController::class, 'marcarComoPago'])->name('premiacoes.marcar_pago');
@@ -181,6 +177,9 @@ Route::middleware(['auth', 'banned', 'trackOnline'])->group(function () {
 });
 
 
+
+Route::get('/email-tracking/open/{trackingId}', [EmailTrackingController::class, 'trackOpen'])->name('email_tracking.open');
+Route::get('/email-tracking/click/{trackingId}', [EmailTrackingController::class, 'trackClick'])->name('email_tracking.click');
 
 require __DIR__ . '/socket.php';
 require __DIR__ . '/auth.php';
